cf6955be3c09ba6fdfd0dc8bfb8acfc08259afd0
Korbinian Kasberger <korbi@taiko.xyz>
2023-09-13 12:59:00 +0200

    add a5 to validation

--- src/utils/isTaikoChain.ts
+++ src/utils/isTaikoChain.ts
@@ -1,7 +1,11 @@
 import { ChainId } from '@uniswap/sdk'
 
 export default function isTaikoChain(chainId?: number): boolean {
-  return [ChainId.TAIKO, ChainId.TAIKO_TESTNET, ChainId.TAIKO_INTERNAL_1, ChainId.TAIKO_TESTNET_L3].includes(
-    chainId as ChainId
-  )
+  return [
+    ChainId.TAIKO,
+    ChainId.TAIKO_TESTNET,
+    ChainId.TAIKO_INTERNAL_1,
+    ChainId.TAIKO_TESTNET_L3,
+    ChainId.TAIKO_TESTNET_JOLNIR
+  ].includes(chainId as ChainId)
 }
